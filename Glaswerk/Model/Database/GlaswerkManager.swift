9fbbb75e606c93611f1ec78ba8f23b20483c1ad0
Bram Goedvriend <braampje.goedvriend@hotmail.be>
2019-12-05 15:35:17 +0100

    Backend change

--- Glaswerk/Model/Database/GlaswerkManager.swift
+++ Glaswerk/Model/Database/GlaswerkManager.swift
@@ -9,7 +9,7 @@
 import Foundation
 
 class GlaswerkManager {
-    let URL = "http://ec2-35-180-181-246.eu-west-3.compute.amazonaws.com:8080/"
+    let URL = "http://bramlab.ga:4444/"
 }
 
 enum BadURLError: Error {
